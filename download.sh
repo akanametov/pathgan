675fa73ba651a104167688282f49ee77ca02ff39
Azamat Kanametov <akkanametov@gmail.com>
2021-05-11 22:53:10 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://github.com/akanametov/pathgan/releases/download/1.0/dataset.zip
+wget https://github.com/akanametov/pathgan/releases/download/2.0/dataset.zip
 unzip dataset.zip
